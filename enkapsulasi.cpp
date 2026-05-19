45049ab030e59e1045920fc374a811bfc7ffc1c0
abil0 <hoenabil6@gmail.com>
2026-05-19 10:43:47 +0700

    class public

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -4,3 +4,12 @@ using namespace std;
 class remoteLampu {
 private:
     string saklarNo[10];
+public:
+    void setsaklarNo( int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getsaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
