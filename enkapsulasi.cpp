d829a19212ab71d4adeacc3aacc94219a6518f26
abil0 <hoenabil6@gmail.com>
2026-05-19 10:48:05 +0700

    main enkapsulasi

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -13,3 +13,21 @@ public:
     }
 };
 
+int main() {
+   remoteLampu lampuRumah;
+    
+    lampuRumah.setsaklarNo(0, "lampu teras rumah");
+    lampuRumah.setsaklarNo(1, "lampu ruang tamu");
+    lampuRumah.setsaklarNo(2, "lampu kamar tidur");
+    lampuRumah.setsaklarNo(3, "lampu dapur");
+
+    cout << lampuRumah.getsaklarNo(0) << endl;
+    cout << lampuRumah.getsaklarNo(1) << endl;
+    cout << lampuRumah.getsaklarNo(2) << endl;
+    cout << lampuRumah.getsaklarNo(3) << endl;
+
+    return 0;
+
+
+
+}
